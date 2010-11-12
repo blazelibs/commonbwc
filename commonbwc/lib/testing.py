92aab87c944f282862a8d4021ad0281f49863eca
Randy Syring <randy.syring@level12.io>
2010-11-12 13:14:14 -0500

    added user_messages() to lib/testing.py
    Extracts user messages from a page, use like:
    
    assert has_message(r.pyq, 'error', 'the message...'), user_messages(r.pyq)

--- commonbwc/lib/testing.py
+++ commonbwc/lib/testing.py
@@ -10,3 +10,11 @@ def has_message(d, sev, message, container_id='user-messages'):
             continue
         if normalizews(li.text()) == '%s : %s' % (sev, message):
             return True
+
+def user_messages(d, container_id='user-messages'):
+    messages = []
+    for li in d('#%s ul li' % container_id):
+        li = d.__class__(li)
+        sev_wrapper = li.find('strong')
+        messages.append((sev_wrapper.text(), li.text()))
+    return messages
